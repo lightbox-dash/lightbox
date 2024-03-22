4145d6b2990138c24f0f00fd94199de99b34159b
zbryikt <tkirby@gmail.com>
2024-03-22 18:18:18 +0800

    use zh-TW only

--- web/static/site/js/gdcfg.js
+++ web/static/site/js/gdcfg.js
@@ -1,1 +1,4 @@
-ldc.register("sitecfg", [], function() { return { i18n: { placeholder: true }}; });
+ldc.register("sitecfg", [], function() { return { i18n: {
+  placeholder: true,
+  cfg: { "supportedLng": ["zh-TW"], fallbackLng: "zh-TW" }
+}}; });
