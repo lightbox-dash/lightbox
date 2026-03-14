33cc075a649ad5bef01585c69670f05c7c999536
SeanGau <rrtw0627@gmail.com>
2026-03-14 13:37:14 +0800

    regen for 26749af

--- static/site/js/gdcfg.js
+++ static/site/js/gdcfg.js
@@ -1,4 +1,11 @@
-ldc.register("sitecfg", [], function() { return { i18n: {
-  placeholder: true,
-  cfg: { "supportedLng": ["zh-TW"], fallbackLng: "zh-TW" }
-}}; });
+ldc.register("sitecfg", [], function () {
+  return {
+    i18n: {
+      placeholder: true,
+      cfg: {
+        supportedLng: ["zh-TW", "en"],
+        fallbackLng: "zh-TW"
+      }
+    }
+  };
+});
